4a4a6efb94ac3c1b55034ebd7c21ef6657841337
Mohamed-Ayman01 <lord39597@gmail.com>
2023-09-25 22:22:09 +0400

    features: select img from files, save img

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
-  sdfd
+let canvas = document.querySelector("canvas.view");
+let ctx = canvas.getContext("2d");
+
+// ! Select image from folders
+let imgInput = document.querySelector("#img-input");
+
+imgInput.addEventListener("change", () => {
+  if (Boolean(document.querySelector("#accepted-img"))) {
+    document.querySelector("#accepted-img").remove();
+  }
+
+  let img = document.createElement("img");
+  img.src = URL.createObjectURL(imgInput.files[0]);
+  img.id = "accepted-img";
+
+  document.body.append(img)
+
+  setTimeout(() => {
+    ctx.drawImage(document.querySelector("#accepted-img"), 0, 0, canvas.width,canvas.height)
+
+  }, 1000)
+})
+
+// ! Save Image
+
+let saveBtn = document.querySelector(".save");
+
+saveBtn.addEventListener("click", (e) => {
+  if (!Boolean(document.querySelector("#accepted-img"))) return e.preventDefault();
+
+  // * show small alert
+  saveBtn.href = canvas.toDataURL("image/jpg");
+})
