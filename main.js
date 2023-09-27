23bcb58f8dd5282b55492909a13991f79512acba
Mohamed-Ayman01 <lord39597@gmail.com>
2023-09-27 21:07:46 +0400

    features: reset filters, sync multiple filters

--- main.js
+++ main.js
@@ -16,6 +16,14 @@ function clearCanvas() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
+function clearCanvasObj() {
+  canvasData.img = "";
+  canvasData.brightness = "";
+  canvasData.saturate = "";
+  canvasData.invert = "";
+  canvasData.grayscale = "";
+}
+
 function drawImg() {
   if (!Boolean(document.querySelector("#accepted-img"))) return;
 
@@ -68,11 +76,11 @@ imgInput.addEventListener("change", () => {
   }
 
   canvasData.img = URL.createObjectURL(imgInput.files[0]);
-
+  
   let img = document.createElement("img");
   img.src = URL.createObjectURL(imgInput.files[0]);
   img.id = "accepted-img";
-
+  
   document.body.append(img);
 
   setTimeout(drawImg, 1000);
@@ -86,7 +94,7 @@ saveBtn.addEventListener("click", (e) => {
   if (!Boolean(document.querySelector("#accepted-img")))
     return e.preventDefault();
 
-  // * show small alert
+  // ! show small alert
   saveBtn.href = canvas.toDataURL("image/jpg");
 });
 
@@ -97,11 +105,7 @@ let removeImageBtn = document.querySelector(".remove-img");
 removeImageBtn.addEventListener("click", () => {
   clearCanvas();
 
-  canvasData.img = "";
-  canvasData.brightness = 0;
-  canvasData.saturation = 0;
-  canvasData.inversion = 0;
-  canvasData.grayscale = 0;
+  clearCanvasObj()
 
   document.querySelector("#accepted-img").remove();
 });
@@ -120,10 +124,58 @@ allFilterInputRange.forEach((input) => {
 
     canvasData[currentFilter] = `${input.value}%`;
 
-    ctx.filter = `${currentFilter}(${canvasData[currentFilter]})`;
+    let listOfFilters = [];
+
+    for (key of Object.keys(canvasData)) {
+      if (canvasData[`${key}`] != "" && key != "img" && key != "currentFilter") {
+        listOfFilters.push(`${key}(${canvasData[key]})`)
+      }
+    }
+
+    console.log(listOfFilters)
+
+    ctx.filter = listOfFilters.join(" ")
 
     drawImg();
 
     input.parentNode.querySelector(".precent").textContent = `${input.value}%`;
   });
+});
+
+// ! Rotate image 
+
+let rotateLeftBtn = document.querySelector(".rotate-left");
+
+// ! fix here
+rotateLeftBtn.addEventListener("click", _ => {
+  clearCanvas();
+
+  ctx.translate(canvas.width / 2, canvas.height / 2);
+  ctx.rotate(90 * Math.PI / 180);
+
+  drawImg()
+
+  ctx.translate(-(canvas.width / 2), -(canvas.height / 2));
+});
+
+let resetFiltersBtn = document.querySelector(".reset-and-file .reset");
+
+resetFiltersBtn.addEventListener("click", _ => {
+  canvasData.brightness = "";
+  canvasData.saturate = "";
+  canvasData.invert = "";
+  canvasData.grayscale = "";
+
+  allFilterInputRange.forEach(input => {
+    let filterName = input.parentNode.getAttribute("data-filter");
+
+    if (filterName == "saturate" || filterName == "brightness") {
+      input.value = 100;
+      input.parentNode.querySelector(".precent").textContent = "100";
+    } else {
+      input.value = 0;
+      input.parentNode.querySelector(".precent").textContent = "0";
+    }
+
+  })
 });
